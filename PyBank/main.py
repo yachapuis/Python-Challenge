b1b30d37c925cd1a482965c0ecc203709b7c61f4
Yves-Andre Chapuis <40185780+yachapuis@users.noreply.github.com>
2018-06-29 19:51:46 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -77,7 +77,7 @@ output = (
 print(output)
 
 # Set path for output file
-txt_output = os.path.join("budget_analysis.md")  
+txt_output = os.path.join("budget_analysis.txt")  
 
 # Export the results to text file
 with open(txt_output, "w") as txt_file:
