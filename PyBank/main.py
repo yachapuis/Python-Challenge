8a888dc7b167266dfb94deaa8cd626f2ab764bc3
Yves-Andre Chapuis <40185780+yachapuis@users.noreply.github.com>
2018-06-29 18:44:51 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,31 +18,25 @@ txt_output = os.path.join("election_analysis.txt")
 # ------------------------------------------
 # Main
 # ------------------------------------------
-# Variables
+# Initiate variable value
 number_months = 0
 total_revenue = 0
 change_revenue = 0
 revenue_memory = 0
-change_revenue_list = []
 average_change = 0
-# month_change = []
-top_profit = ["", 0]
-top_losses = ["", 9999999999999999999]
+# Initiate variable list
+change_revenue_list = []
+top_profit_list = ["", 0]
+top_losses_list = ["", 9999999999999999999]
 
 # Read the csv file and convert it into lists
 with open(csv_path) as budget_data:
     csv_reader = csv.reader(budget_data)
 
-    # Skip first row
+    # Skip the first row of the csv_reader file
     next(csv_reader) 
 
-# Extract first row to avoid appending to net_change_list
-#   first_row = next(reader)
-#   total_months = total_months + 1
-#   total_net = total_net + int(first_row[1])
-#   prev_net = int(first_row[1])
-
-    # Loop
+    # Read all rows of the csv_reader file in a loop
     for row in csv_reader:
 
         # Count months number and total of revenue
@@ -56,13 +50,13 @@ with open(csv_path) as budget_data:
 
         # Calculate the greatest increase in profits (date and amount) over the entire period
         if change_revenue > top_profit[1]:
-            top_profit[1] = change_revenue
-            top_profit[0] = row[0]
+            top_profit_list[1] = change_revenue
+            top_profit_list[0] = row[0]
 
          # Calculate greatest decrease in losses (date and amount) over the entire period
         if change_revenue < top_losses[1]:
-            top_losses[1] = change_revenue
-            top_losses[0] = row[0]
+            top_losses_list[1] = change_revenue
+            top_losses_list[0] = row[0]
 
 # Calculate the average change in revenue between months over the entire period
 average_change = sum(change_revenue_list) / len(change_revenue_list)
@@ -77,8 +71,8 @@ output = (
     f"Total Number of Months: {number_months}\n"
     f"Total Amount of Revenue: ${total_revenue}\n"
     f"Average Change in Revenue: ${average_change:.2f}\n"
-    f"Greatest Increase in Profits: {top_profit[0]} (${top_profit[1]})\n"
-    f"Greatest Decrease in Profits: {top_losses[0]} (${top_losses[1]})\n")
+    f"Greatest Increase in Profits: {top_profit_list[0]} (${top_profit_list[1]})\n"
+    f"Greatest Decrease in Profits: {top_losses_list[0]} (${top_losses_list[1]})\n")
 
 # Print the output (to terminal)
 print(output)
